9c8fbc1251fbba23a10200c0e907d8d19dd90467
pankajsoor09 <pankajsoor09@gmail.com>
2024-06-11 19:37:45 +0530

    Update example2.java

--- example2.java
+++ example2.java
@@ -3,7 +3,8 @@ package com.firstprogram;
 public class example2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); 
+		System.out.println("Hello World"); 
 
 	}
 
